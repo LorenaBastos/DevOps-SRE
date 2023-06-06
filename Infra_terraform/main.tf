8d72ff065b666764991872746e19da4c150ef0cb
Lorena Bastos <42914289+LorenaBastos@users.noreply.github.com>
2023-06-06 16:26:40 -0300

    Update main.tf

--- Infra_terraform/main.tf
+++ Infra_terraform/main.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  credentials = file("path/to/service-account-key.json")
-  project     = "<your-project-id>"
-  region      = "<your-preferred-region>"
+  credentials = file("service-account-key.json")
+  project     = "<project-id>"
+  region      = "us-central1"
 }
 
 resource "google_container_cluster" "my_cluster" {
   name               = "my-gke-cluster"
-  location           = "<your-preferred-region>"
+  location           = "us-central1-a"
   initial_node_count = 1
   node_config {
     machine_type = "n1-standard-2"
